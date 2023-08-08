c3e72096bedac4f64732e995be54db8bbe1535c9
LSQFourSixSeven <luosiqi647@126.com>
2023-08-09 03:34:43 +0800

    添加gets() & puts() 函数

--- C语言输入输出/C语言输入输出.c
+++ C语言输入输出/C语言输入输出.c
@@ -16,12 +16,29 @@ void main(void)
 	这个函数在同一个时间内只会输出一个单一的字符。
 
 	推荐在循环内使用这个两个方法，以便在屏幕上输入输出多个字符。*/
-	unsigned char input_gc;
-	printf("请输入一个字符:");//显示输入提示
-	input_gc = getchar();//获取字符
-	printf("\nYou entered: ");//显示输出提示
-	putchar(input_gc);//输出所输入的字符
+	printf("getchar() & putchar() 函数");
+	int input_gc;
+	printf("请输入一个字符（多于一个字符将读取第一个字符）：");//显示输入提示
+	input_gc = getchar();//获取字符，返回整形
+	printf("\n你输入了：: ");//显示输出提示
+	putchar(input_gc);//输出输入所得以整形表示的字符
 	printf("\n");
+
+	/*gets() & puts() 函数*/
+	/*
+	gets(char* s) 函数从 stdin 读取一行到传入的字符型变量，直到一个终止符或 EOF。
+	puts(const char *s) 函数把传入的字符串和一个尾随的换行符写入到 stdout。
+	*/
+	printf("gets() & puts() 函数");
+	char gets_input[20];//定义能存下20个字符的变量作为缓冲区
+
+	printf("请输入字符 :");
+	gets(gets_input);//读取一行字符并放入所传入的变量
+
+	printf("\n你输入了: ");
+	puts(gets_input);//输出用gets函数所读取的字符
+
+
 
 
 
