5a34b6af80438802b1e8a148c50cb04b0ef86af0
LSQFourSixSeven <luosiqi647@126.com>
2023-08-08 23:45:41 +0800

    更新：完善printf用法中对stdio.h头文件名的分析创建

--- 示例：printf用法.c
+++ 示例：printf用法.c
@@ -1,4 +1,11 @@
-#include "stdio.h";
+#include "stdio.h";//包含“stdio.h”头文件
+/*
+stdio文件名解析：
+"std": 全称standard，译为标准
+"io" : 全称input output，分别译为输入 输出
+"h"  : 全称headers， 译为头文件
+则stdio.h全称为standard input output.header，译为标准输入输出头文件
+*/
 void main(void)
 {
 	printf("ending here\r\n");
