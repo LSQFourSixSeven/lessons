09f949b501b68c9274e2491aaf471ce5485b1845
LSQFourSixSeven <luosiqi647@126.com>
2023-08-08 18:12:22 +0800

    创建：创建C语言程序结构 创建：示例：printf用法

---
+++ C语言程序结构.c
@@ -0,0 +1,9 @@
+/*此部分代码被称为头文件*/
+#include "stdio.h"; //预处理文件，包含了stdio.h这一头文件
+/*以上部分代码被称为头文件*/
+
+void main(void) //定义返回类型为void且名为main的函数，main在程序中会作为函数，所有程序会从main函数开始执行
+{
+	printf("hello world");//使用printf函数，用于输出括号中的hello world
+	return;//返回void（空）值，同时函数会在此结束
+}
---
+++ 示例：printf用法.c
@@ -0,0 +1,6 @@
+#include "stdio.h";
+void main(void)
+{
+	printf("ending here\r\n");
+	return;
+}
