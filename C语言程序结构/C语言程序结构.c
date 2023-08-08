ed8aa79466ee8618b46abdb6cb5fa00828af107f
LSQFourSixSeven <luosiqi647@126.com>
2023-08-09 02:40:30 +0800

    修改解决方案结构

--- C语言程序结构/C语言程序结构.c
+++ C语言程序结构/C语言程序结构.c
@@ -1,9 +1,9 @@
 /*此部分代码被称为头文件*/
-#include "stdio.h"; //预处理文件，引入了stdio.h这一头文件
+#include <stdio.h>//预处理文件，引入了"C语言输入输出.c"这C语言文件
 /*以上部分代码被称为头文件*/
 
 void main(void) //定义返回类型为void且名为main的函数，main在程序中会作为函数，所有程序会从main函数开始执行
 {
-	printf("hello world");//使用printf函数，用于输出括号中的hello world
+	printf("hello world");//所使用的函数，用于打印"hello world"
 	return;//返回void（空）值，同时函数会在此结束
 }
--- C语言输入输出/C语言输入输出.c
+++ C语言输入输出/C语言输入输出.c
@@ -1,4 +1,4 @@
-#include "stdio.h";//引入“stdio.h”头文件
+#include "stdio.h" //引入“stdio.h”头文件
 /*
 stdio文件名解析：
 "std": 全称standard，译为标准
@@ -9,8 +9,6 @@ stdio文件名解析：
 void main(void)
 {
 	/* C 语言中的 I/O (输入/输出) 通常使用 printf() 和 scanf() 两个函数。*/
-	//scanf() 函数用于从标准输入（键盘）读取并格式化
-	scanf("");//此函数在"stdio.h"头文件中申明
 	//printf() 函数发送格式化输出到标准输出（屏幕）。
 	printf("ending here\r\n");//此函数在"stdio.h"头文件中申明
 	return;
